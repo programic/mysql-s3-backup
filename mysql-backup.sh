52ebfe4c2f733084de6f72998a49ad4a6a9ea841
Remco Wolvenne <r.wolvenne@programic.com>
2021-11-28 10:04:40 +0100

    Some nice comments

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -17,29 +17,34 @@ for server_encoded in $(echo "${config}" | jq -r '.servers[] | @base64'); do
 
   s3_bucket=$(echo "${server}" | jq -r '.s3_bucket')
   exclude=$(echo "${server}" | jq -r '.exclude')
-  params="--host=$(echo "${server}" | jq -r '.host') \
+  login_params="--host=$(echo "${server}" | jq -r '.host') \
     --port=$(echo "${server}" | jq -r '.port') \
     --user=$(echo "${server}" | jq -r '.user') \
     --password=$(echo "${server}" | jq -r '.password')"
 
-  # Get databases
+  # Get available databases, skip excluded
   exclude_sql="('$(echo "${exclude}" | sed -e "s/\s\+/','/g")')"
-  databases=$(mysql ${params} \
+  databases=$(mysql ${login_params} \
     -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ${exclude_sql}" \
     -s --skip-column-names)
 
+  # Loop databases
   for database in ${databases}; do
     dump_file=$(mktemp)
 
-    mysqldump ${params} --set-gtid-purged=OFF --triggers --routines --events --single-transaction --quick \
+    # Backup single database
+    mysqldump ${login_params} \
+      --set-gtid-purged=OFF --triggers --routines --events --single-transaction --quick \
       --databases ${database} \
     | gzip > ${dump_file}
 
+    # Upload backup to AWS S3
     aws s3 mv ${dump_file} s3://${s3_bucket}/${backup_date}/${database}.sql.gz
   done
 
 done
 
+# Send pingback after backup is finished
 pingback=$(echo "${config}" | jq -r '.pingback')
 pingback_url=$(echo "${pingback}" | jq -r '.url')
 pingback_retry=$(echo "${pingback}" | jq -r '.retry')
