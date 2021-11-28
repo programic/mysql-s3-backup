e6097f22bb817ec612fa77e8fa5b9f9f43102333
Remco Wolvenne <r.wolvenne@programic.com>
2021-11-28 09:46:01 +0100

    Improve codestyle and integrate pingback.

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -2,40 +2,48 @@
 
 set -e
 
-CONFIG=$(cat config.json)
-AWS=$(echo "${CONFIG}" | jq -r '.aws')
-BACKUP_DATE=$(date +"%Y-%m-%dT%H:%M:%S%Z")
+config=$(cat config.json)
+aws=$(echo "${config}" | jq -r '.aws')
+backup_date=$(date +"%Y-%m-%dT%H:%M:%S%Z")
 
 # Set environment vars for awscli
-export AWS_ACCESS_KEY_ID=$(echo "${AWS}" | jq -r '.access_key_id')
-export AWS_SECRET_ACCESS_KEY=$(echo "${AWS}" | jq -r '.secret_access_key')
-export AWS_DEFAULT_REGION=$(echo "${AWS}" | jq -r '.region')
+export AWS_ACCESS_KEY_ID=$(echo "${aws}" | jq -r '.access_key_id')
+export AWS_SECRET_ACCESS_KEY=$(echo "${aws}" | jq -r '.secret_access_key')
+export AWS_DEFAULT_REGION=$(echo "${aws}" | jq -r '.region')
 
 # Loop servers
-for SERVER_ENCODED in $(echo "${CONFIG}" | jq -r '.servers[] | @base64'); do
-  SERVER=$(echo "${SERVER_ENCODED}" | base64 -d)
+for server_encoded in $(echo "${config}" | jq -r '.servers[] | @base64'); do
+  server=$(echo "${server_encoded}" | base64 -d)
 
-  S3_BUCKET=$(echo "${SERVER}" | jq -r '.s3_bucket')
-  EXCLUDE=$(echo "${SERVER}" | jq -r '.exclude')
-  PARAMS="--host=$(echo "${SERVER}" | jq -r '.host') \
-    --port=$(echo "${SERVER}" | jq -r '.port') \
-    --user=$(echo "${SERVER}" | jq -r '.user') \
-    --password=$(echo "${SERVER}" | jq -r '.password')"
+  s3_bucket=$(echo "${server}" | jq -r '.s3_bucket')
+  exclude=$(echo "${server}" | jq -r '.exclude')
+  params="--host=$(echo "${server}" | jq -r '.host') \
+    --port=$(echo "${server}" | jq -r '.port') \
+    --user=$(echo "${server}" | jq -r '.user') \
+    --password=$(echo "${server}" | jq -r '.password')"
 
   # Get databases
-  EXCLUDE_SQL="('$(echo "${EXCLUDE}" | sed -e "s/\s\+/','/g")')"
-  DATABASES=$(mysql ${PARAMS} \
-    -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ${EXCLUDE_SQL}" \
+  exclude_sql="('$(echo "${exclude}" | sed -e "s/\s\+/','/g")')"
+  databases=$(mysql ${params} \
+    -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ${exclude_sql}" \
     -s --skip-column-names)
 
-  for DATABASE in ${DATABASES}; do
-    DUMP_FILE=$(mktemp)
+  for database in ${databases}; do
+    dump_file=$(mktemp)
 
-    mysqldump ${PARAMS} --set-gtid-purged=OFF --triggers --routines --events --single-transaction --quick \
-      --databases ${DATABASE} \
-    | gzip > ${DUMP_FILE}
+    mysqldump ${params} --set-gtid-purged=OFF --triggers --routines --events --single-transaction --quick \
+      --databases ${database} \
+    | gzip > ${dump_file}
 
-    aws s3 mv ${DUMP_FILE} s3://${S3_BUCKET}/${BACKUP_DATE}/${DATABASE}.sql.gz
+    aws s3 mv ${dump_file} s3://${s3_bucket}/${backup_date}/${database}.sql.gz
   done
 
 done
+
+pingback=$(echo "${config}" | jq -r '.pingback')
+pingback_url=$(echo "${pingback}" | jq -r '.url')
+pingback_retry=$(echo "${pingback}" | jq -r '.retry')
+
+if [[ $pingback_url ]]; then
+  curl --retry ${pingback_retry} ${pingback_url}
+fi
